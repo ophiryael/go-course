435e675a44b1b10fdf33e0acce3caeea29d7539b
Ophir Yael <ofir.yael@gmail.com>
2022-08-12 14:46:59 +0300

    Add interface example

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// custom logic for generating an english greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	// custom logic for generating a spanish greeting
+	return "Hola!"
+}
